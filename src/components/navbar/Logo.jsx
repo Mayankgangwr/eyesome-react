a1c2fe01c990f5c82e642d961c1f9ebffd50b058
DESKTOP-M9DKGD4\DELL <iammayankgangwarbly@gmail.com>
2023-08-06 12:10:33 +0530

    done

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/">
-      <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition">
+      <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition" style={{ fontSize: '16px' }}>
         Kart Electro
       </div>
     </Link>
